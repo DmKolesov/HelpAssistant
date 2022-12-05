0ef014d4350ea132be0f69d9e79745c23a68ff37
Dima Kolesov <kolesov.public@gmail.com>
2022-12-05 19:19:44 +0300

    init commit

---
+++ StartNewProject/StartNewProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StartNewProject
+//
+//  Created by TX 3000 on 05.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
